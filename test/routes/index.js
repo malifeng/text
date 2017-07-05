67a52df8e703f445aff4f9c333a5a33a5fd2ad3a
马丽峰 <1005514873@qq.com>
2017-07-05 13:53:48 +0800

    Changed some files

--- test/routes/index.js
+++ test/routes/index.js
@@ -8,4 +8,22 @@ router.get('/', async function (ctx, next) {
   await ctx.render('index', {
   });
 })
+
+router.get('/admin', async function (ctx, next) {
+    ctx.state = {
+        title: 'koa2 title'
+    };
+
+    await ctx.render('index', {
+    });
+})
+
+router.get('/teacher', async function (ctx, next) {
+    ctx.state = {
+        title: 'koa2 title'
+    };
+
+    await ctx.render('index', {
+    });
+})
 module.exports = router;
